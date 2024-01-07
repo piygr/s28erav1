4f894325c43d4ee8613b190ef2f651c586ce53bd
piygr <grover1piyush@gmail.com>
2024-01-07 23:58:37 +0530

    updated config

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ def get_config():
         epoch_steps=10,
         seq_len=512,
         preload=False,
-        feed_url='https://b9f6-35-196-149-33.ngrok-free.app/generate'
+        feed_url='https://a53c-34-91-107-251.ngrok-free.app/generate'
     )
